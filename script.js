6cea51c17b388c5e38e2c57d13b4ea49638f3cde
Nale-kunal <nalekunal343@gmail.com>
2025-08-06 21:21:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+function daysOfAYear(year) {
+  if ((year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0)) {
+    return 366;
+  }
+  return 365;
+}
+
+
+let input = prompt("Enter a year:");
+let year = Number(input);
+  
+if (year >= 1 && year <= 9999) {
+  alert("Number of days in year " + year + " is: " + daysOfAYear(year));
+} else {
+  alert("Please enter a valid year between 1 and 9999.");
+}

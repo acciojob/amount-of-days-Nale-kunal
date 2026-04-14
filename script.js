cbdc7b13e87dc60b34f8558eef7c6aee59d15c34
Nale-kunal <nalekunal343@gmail.com>
2026-04-14 23:07:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+	let leap=true;
+	if(year%400==0) return 366;
+	else if{
+		if(year%4==0 && year%100!=0) return 366;
+	}
+	else{
+		return 365;
+	}
+}
+
+
+
